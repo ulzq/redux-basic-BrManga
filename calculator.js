8c512b714d4549119a40677b5512f83768a32dd1
Bora Manga <bora.mng525@gmail.com>
2019-11-25 12:35:23 +0100

    Commitment

--- calculator.js
+++ calculator.js
@@ -1,22 +1,33 @@
+let { createStore } = require("redux");
 
-let { createStore } = require('redux')
-
-let reducer = (state=1,{type,value=1})=>{
+let reducer = (state = 1, { type, value = 1 }) => {
   // user wants to change something
-  switch (type){
-    case "ADD": return state + value;
-    case "SUB": return state - value;
-    default: return state;
-}}
+  switch (type) {
+    case "ADD":
+      return state + value;
+    case "SUB":
+      return state - value;
+    case "DIV":
+      return state / value;
+    case "MUL":
+      return state * value;
+    default:
+      return state;
+  }
+};
 
-let store = createStore( reducer )
+let store = createStore(reducer);
 
-store.subscribe( ()=>{
+store.subscribe(() => {
   // something on the state has changed
-  console.log('stateIs',store.getState())
-})
+  console.log("stateIs", store.getState());
+});
 
-store.dispatch({ type:"ADD" })
-store.dispatch({ type:"ADD", value:2 })
-store.dispatch({ type:"SUB" })
-store.dispatch({ type:"MOO" })
+store.dispatch({ type: "ADD" });
+store.dispatch({ type: "ADD", value: 2 });
+store.dispatch({ type: "DIV", value: 2 });
+store.dispatch({ type: "MUL", value: 3 });
+store.dispatch({ type: "MUL", value: 3 });
+["ADD", "MUL"].map(action => {
+  store.dispatch({ type: action, value: 2 });
+});
